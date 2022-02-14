2c64f34a117000de0171436040cf05c714c87ff6
danoux10 <danybarbe10@gmail.com>
2022-02-14 07:22:48 +0100

    debut icon view

---
+++ script/icon.js
@@ -0,0 +1,19 @@
+// anchor show/hidden form icon_add
+var toggleAdd = document.getElementById('showAddIcon');
+var viewAdd = document.getElementById('iconFormAdd');
+
+toggleAdd.classList.add('empty');
+
+function toggleViewAdd(){
+    if (toggleAdd.classList.contains('empty')){
+        toggleAdd.classList.add('active');
+        toggleAdd.classList.remove('empty');
+        viewAdd.classList.add('hidden');
+    }else{
+        toggleAdd.classList.remove('active');
+        toggleAdd.classList.add('empty');
+        viewAdd.classList.remove('hidden');
+    }
+}
+
+toggleAdd.addEventListener('click',toggleViewAdd);
--- view/icon.php
+++ view/icon.php
@@ -6,7 +6,19 @@
 ?>
 
 <div id="icon_add">
-    <form action="" method="post" enctype="multipart/form-data">
+    <div id="showAddIcon">
+        <span id="iconHidden">
+         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 3.6">
+          <path id="Icon_ionic-md-add" data-name="Icon ionic-md-add" d="M33.75,22.05h-27v-3.6h27Z" transform="translate(-6.75 -18.45)" />
+        </svg>
+        </span>
+        <span id="iconShow">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27">
+              <path id="Icon_ionic-md-add" data-name="Icon ionic-md-add" d="M33.75,22.05H22.05v11.7h-3.6V22.05H6.75v-3.6h11.7V6.75h3.6v11.7h11.7Z" transform="translate(-6.75 -6.75)"/>
+            </svg>
+        </span>
+    </div>
+    <form action="" method="post" id="iconFormAdd" enctype="multipart/form-data">
         <fieldset>
             <legend>Add Icon</legend>
             <input type="file" name="icon" accept=".svg" required>
